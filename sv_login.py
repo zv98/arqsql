d4e7901e7293ee67f533e0d3b326f1714aebe251
Jun <coni.zv98@gmail.com>
2021-07-11 05:12:51 -0400

    Update sv_login.py

--- sv_login.py
+++ sv_login.py
@@ -11,7 +11,7 @@ s.sendall(bytes('00010sinitlogin','utf-8'))
 
 #def recibir(sock, addr):
 print("Ingresando a la cuenta de usuario")
-while True:
+while s.recv(4096):
     datos = s.recv(4096)
     if datos.decode('utf-8').find('login'):
         datos = datos[10:]
